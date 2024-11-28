a904171daf32fa02c0311bf5f1014f59018652d7
moffa89 <85792173+moffa89@users.noreply.github.com>
2024-11-28 02:22:35 +0100

    Update OneUi 6.1 Tweaks.bat

--- OneUi 6.1 Tweaks.bat
+++ OneUi 6.1 Tweaks.bat
@@ -50,7 +50,7 @@ echo Improving Power Management on Phones
 adb shell settings put global sem_enhanced_cpu_responsiveness 0
 adb shell settings put global enhanced_processing 0
 adb shell settings put global app_standby_enabled 1
-adb shell settings put global adaptive_battery_management_enabled 0
+adb shell settings put global adaptive_battery_management_enabled 1
 adb shell settings put global app_restriction_enabled true
 adb shell settings put global automatic_power_save_mode 0
 
